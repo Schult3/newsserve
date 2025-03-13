7447b54a862ee3fc69ce6829b42ab9b96bed0cfb
Schulte, Stefan <stefan.schulte2@thyssenkrupp-steel.com>
2025-03-13 10:09:09 +0100

    Small Text

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ foreach ($latest_items as $item) {
     $dt = new DateTime();
     $dt->setTimezone(new DateTimeZone('Europe/Berlin'));
     $dt->setTimestamp( $item[ "pubDate" ] );
-    $tmp[] = "**" .$item[ "title" ] ."**<br>" .$item[ "description" ] . " (" .$dt->format( 'H:i' ) .")";
+    $tmp[] = "**" .$item[ "title" ] ."**<br><sub><sup>" .$item[ "description" ] . "</sup></sub> (" .$dt->format( 'H:i' ) .")";
     $i++;
 
 }
