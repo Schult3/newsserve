dc62b83a5639586f0a1f569c56ae799286f40932
Schulte, Stefan <stefan.schulte2@thyssenkrupp-steel.com>
2025-03-13 10:20:29 +0100

    rtrim

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ foreach ($latest_items as $item) {
     $dt = new DateTime();
     $dt->setTimezone(new DateTimeZone('Europe/Berlin'));
     $dt->setTimestamp( $item[ "pubDate" ] );
-    $tmp[] = "**" .$item[ "title" ] ."**<br><small>" .$item[ "description" ] . " (" .$dt->format( 'H:i' ) .")</small>";
+    $tmp[] = "**" .rtrim( $item[ "title" ] ) ."**<br><small>" .$item[ "description" ] . " (" .$dt->format( 'H:i' ) .")</small>";
     $i++;
 
 }
